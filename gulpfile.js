db50a4ade995f59dd8933ad73d9bb62958f52d74
Krusha Patel <krushampatel@yahoo.com>
2020-12-03 16:42:31 -0600

    part2 create gulp minify, concat and sourcemaps tasks for css and js, create optimize images task for all images

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const {src, dest, series, parallel} = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+
+function htmlTask(){
+  return src('src/*html')
+  .pipe(dest('dist'))
+}
+
+function scriptsTask(){
+  return src('src/js/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(terser())
+  .pipe(sourcemaps.write())
+  .pipe(concat('all.js'))
+  .pipe(dest('dist/js'))
+}
+
+function stylesTask(){
+  return src('src/css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
+  .pipe(sourcemaps.write())
+  .pipe(concat('all.css'))
+  .pipe(dest('dist/css'))
+}
+
+function imagesTask(){
+  return src('src/images/*')
+  .pipe(imagemin())
+  .pipe(dest('dist/images'))
+}
+
+exports.html = htmlTask;
+exports.scripts = scriptsTask;
+exports.images = imagesTask;
+exports.styles = stylesTask;
+exports.images = imagesTask;
+exports.default = series(htmlTask, imagesTask,scriptsTask, stylesTask);
